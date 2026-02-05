88015f82debd7b46c559ca187339fca798410687
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2026-02-05 11:00:20 -0500

    feat(simulation): add voltage-based consumption variability to fuel dispenser system.

--- back/services/simulation_service.py
+++ back/services/simulation_service.py
@@ -11,6 +11,7 @@ from systems.office_and_general_services import get_hourly_office_and_general_se
 from systems.air_conditioning_system import get_hourly_air_conditioning_consumption
 from systems.refrigeration import get_hourly_refrigeration_consumption
 from systems.submersible_pump_system import get_hourly_submersible_pump_system_consumption
+from systems.fuel_dispenser_system import get_hourly_fuel_dispenser_system_consumption
 
 
 SYSTEMS_CONSUMPTION_PER_HOUR = {
@@ -147,6 +148,12 @@ def generate_hourly_consumption(timestamp):
                 elif system == "submersible_pump_system":
 
                     real_consumption = get_hourly_submersible_pump_system_consumption()
+
+                elif system == "fuel_dispenser_system":
+
+                    real_consumption = get_hourly_fuel_dispenser_system_consumption()
+
+                
 
                 else:
 
---
+++ back/systems/fuel_dispenser_system.py
@@ -0,0 +1,39 @@
+
+
+"""
+References:
+
+CREG Resolution 024 of 2005 (Colombia):
+https://gestornormativo.creg.gov.co/Publicac.nsf/1c09d18d2d5ffb5b05256eee00709c02/7ef77a545ceb66680525785a007a6b88/$FILE/Creg024-2005.pdf
+
+ANSI C84.1 Voltage Tolerance Standard (USA):
+https://voltage-disturbance.com/voltage-quality/voltage-tolerance-standard-ansi-c84-1/
+"""
+
+import random
+
+FUEL_DISPENSER_SYSTEM_NOMINAL_POWER_KWH = 0.0275
+FUEL_DISPENSER_SYSTEM_NOMINAL_VOLTAGE = 220
+FUEL_DISPENSER_SYSTEM_MIN_VOLTAGE = 209
+FUEL_DISPENSER_SYSTEM_MAX_VOLTAGE = 231
+FUEL_DISPENSER_SYSTEM_EQUIVALENT_HOURLY_DURATION = 2.05 / 24
+
+
+def get_hourly_fuel_dispenser_system_consumption():
+
+    voltage = random.uniform(
+
+        FUEL_DISPENSER_SYSTEM_MIN_VOLTAGE,
+        FUEL_DISPENSER_SYSTEM_MAX_VOLTAGE
+
+    )
+
+    real_consumption = (
+
+        FUEL_DISPENSER_SYSTEM_NOMINAL_POWER_KWH
+        * (voltage / FUEL_DISPENSER_SYSTEM_NOMINAL_VOLTAGE)
+        * FUEL_DISPENSER_SYSTEM_EQUIVALENT_HOURLY_DURATION
+        
+    )
+
+    return real_consumption
